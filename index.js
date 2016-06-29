b0557fdca0b9ecaf6fb28c09d455da6d8e68bfe7
Cody Barber <cody.n.barber@gmail.com>
2016-06-28 21:23:23 -0400

    initial commit of socket.io chat app

---
+++ index.js
@@ -0,0 +1,38 @@
+var express = require('express');
+var app = express();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+
+app.get('/', function(req, res) {
+  res.sendFile(__dirname + '/index.html');
+});
+
+//connection event is triggered when the io() is called on the client side
+io.on('connection', function(clientSocket) {
+  console.log('a user connected');
+  
+  clientSocket.on('client-join', function(username) {
+    clientSocket.username = username;
+    clientSocket.broadcast.emit('join', username);
+  });
+
+  clientSocket.on('disconnect', function() {
+    io.emit('leave', clientSocket.username);
+  });
+
+  clientSocket.on('message-to-server', function(msg) {
+    clientSocket.broadcast.emit('message-to-client', msg);
+  });
+
+  clientSocket.on('start-type', function(username) {
+    clientSocket.broadcast.emit('start-type', username);
+  });
+
+  clientSocket.on('end-type', function(username) {
+    clientSocket.broadcast.emit('end-type', username);
+  });
+});
+
+http.listen(3000, function() {
+  console.log('listening on *:3000');
+});
